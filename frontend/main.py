7e1f367a595b368482d6f622c8ab8be842509cc3
Lucaslllll <lucascosta12367@gmail.com>
2022-08-08 17:18:36 -0300

    update aplicativo de aprendizado

--- frontend/main.py
+++ frontend/main.py
@@ -2,6 +2,13 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
+from kivy.uix.behaviors.button import ButtonBehavior
+from kivy.uix.label import Label
+from kivy.graphics import Color, Rectangle, Ellipse
+from kivy.properties import ListProperty
+
+
+
 
 class Gerenciador(ScreenManager):
     pass
@@ -27,6 +34,50 @@ class Ajuda(Screen):
 
 class Menu(Screen):
     pass
+
+
+# fazer um botão sem ser no .kv
+class Botao(ButtonBehavior, Label):
+    cor = ListProperty([0.1, 0.5, 0.7, 1])
+    cor2 = ListProperty([0.1, 1, 0.2, 1])
+
+    def __init__(self, **kwargs):
+        # herdará de buttonbehavior e label
+        super(Botao, self).__init__(**kwargs)
+        self.atualizar()
+
+
+    def on_pos(self, *args):
+        self.atualizar()
+
+    def on_size(self, *args):
+        self.atualizar()
+
+
+    def on_press(self, *args):
+        # atribuição simutânea
+        self.cor, self.cor2 = self.cor2, self.cor
+
+    def on_release(self, *args):
+        self.cor = self.cor2
+
+    # no momento que eu crio uma variavel ela tambem ganha sua função de evento
+    def on_cor(self, *args):
+        self.atualizar()
+
+    def atualizar(self, *args):
+        self.canvas.before.clear()
+        with self.canvas.before:
+            Color(rgba=self.cor)
+            Ellipse(size=(self.height, self.height),
+                    pos=(self.pos)
+                )
+            Ellipse(size=(self.height, self.height),
+                    pos=(self.x+self.width-self.height, self.y)
+                )
+            Rectangle(size=(self.width-self.height, self.height),
+                      pos=(self.x+self.height/2.0, self.y)
+                )
 
 
 
@@ -73,3 +124,20 @@ class FrontAdmin(App):
 
 
 FrontAdmin().run()
+
+
+
+
+# <Botao@ButtonBehavior+Label>:
+#     canvas.before:
+#         Color:
+#             rgba: 0.2, 0.6, 0.3, 1
+#         Ellipse:
+#             pos: self.pos
+#             size: self.height, self.height
+#         Ellipse:
+#             pos: self.x+self.width-self.height, self.y
+#             size: self.height, self.height
+#         Rectangle:
+#             pos: self.x + self.height/2.0, self.y
+#             size: self.width-self.height, self.height
